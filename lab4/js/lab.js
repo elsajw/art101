9258f9a1bd3c1e3231750da56262bf3c0322295a
elsajw <williamselsa.j@gmail.com>
2024-04-23 20:23:10 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 Rock Paper Scissors
+// Author: Elsa Williams
+// Date: April 23, 2024
 
 // Constants
 
@@ -11,6 +11,18 @@ function myFunction(param1, param2) {
   // some code here
   // return results;
 }
+
+//Creating a Rock, Paper, Scissors game:
+  //Start each turn with a countdown
+    //Rock, paper, scissors, shoot
+  //Accept user input
+    //Either rock, paper, or scissors
+  //Randomize computer choice
+    //Either rock, paper, or scissors
+  //Evaluate winner
+    //Rock beats scissors, scissors beats paper, paper beats rock
+
+
 
 function main() {
   console.log("Main function started.");
