a20800a8a4b1e1682d8cb09c5ec87c15b4a4f1cf
elsajw <williamselsa.j@gmail.com>
2024-05-06 21:14:11 -0700

    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,36 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author:    Elsa Williams
+ * Created:   05.06.2024
+ **/
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//creating the function
+function addsThree(x){
+  return (x + 3);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//test the function
+console.log("What's 3 + 3?", addsThree(3));
+console.log("what's 6 + 3?", addsThree(6));
 
-// let's get this party started
-main();
+//creating the array
+array = [27, -3, 42, 0, 200, 1980];
+console.log("My array: ", array);
+
+let result = array.map(addsThree);
+//should return [30, 0, 45, 3, 203, 1983]
+console.log("Adding 3 to each value in the array:", result);
+
+//second function
+let results = array.map(function(x){
+  return x * 2;
+});
+//should return [54, -6, 84, 0, 400, 3960]
+console.log("Multiplying the array's values by two: ", results);
+
+
+
+ // Your map results data
+ let mapResults = "My map results here! ";
+
+ // Use jQuery to select the element by its ID and set the HTML content
+ $("#output").html(mapResults);
