3a64255c876c2448c783352b1c7f3a844e19712b
elsajw <williamselsa.j@gmail.com>
2024-05-23 20:52:14 -0700

    Lab 12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,21 +1,33 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author:    Elsa Williams
+ * Created:   23.05.2024
+ **/
 
-// Constants
+//Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
+//depending on length mod 4
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else (mod == 3) {
+    return "Hufflepuff"
+  }
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//creating the button
+let myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  let name = document.getElementById("input").value;
+  let house = sortingHat (name);
+  newText = <p>The Sorting Hat has sorted you into " + house +"</p>;
+  document.getElementById("output").innerHTML = newText;
+})
