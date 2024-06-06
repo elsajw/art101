ce3464626fcf06b9f543577035a7bd18b2d739e8
elsajw <williamselsa.j@gmail.com>
2024-06-06 10:42:50 -0700

    Lab 16- final lab! Yay!

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,21 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author:    Elsa Williams
+ * Created:   June 6 2024
+ *
+ **/
 
-// Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Using the core $.ajax() method
+$.ajax({
+  // The URL for the request (from the api docs)
+  url: "https://xkcd.com/info.0.json",
+  // The data to send (will be converted to a query string)
+  data: { 
+          // here is where any data required by the api 
+          //   goes (check the api docs)
+          id: 123,
+          api_key: "blahblahblah",
+        },
+  // Whether this is a POST or GET request
+  type: "GET",
+  // The type of data we expect back
+  dataType : "json",
+  // What do we do when the api call is successful
+  //   all the action goes in here
+  success: function(data) {
+      // do stuff
+      console.log(data);
+  },
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
+  }
+})
